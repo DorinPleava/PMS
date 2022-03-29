c59c191e76af64d2ed6f4c421eeb3e601a9c911a
Pleava, Dorin (PERO-SP) <dorin.pleava@porsche-engineering.ro>
2022-03-29 15:16:52 +0300

    Added some ui with Jinja

--- backend/main.py
+++ backend/main.py
@@ -1,9 +1,21 @@
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles #new
 from core.config import settings
+from apis.general_pages.route_homepage import general_pages_router
 
-app = FastAPI(title=settings.PROJECT_NAME, version=settings.PROJECT_VERSION)
+
+def include_router(app):
+    app.include_router(general_pages_router)
 
 
-@app.get("/")
-def hello_api():
-    return {"msg": "Hello API"}
+def configure_static(app):  #new
+    app.mount("/static", StaticFiles(directory="static"), name="static")
+
+
+def start_application():
+    app = FastAPI(title=settings.PROJECT_NAME,version=settings.PROJECT_VERSION)
+    include_router(app)
+    configure_static(app) #new
+    return app
+
+app = start_application()
